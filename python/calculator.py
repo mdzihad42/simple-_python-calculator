6a040966ff002afbac53738476abc8203f98ee6e
MD ZIHAD HOSSAIN <mdzihad01793561142@gmail.com>
2025-12-31 15:21:31 +0600

    simple calculator

---
+++ python/calculator.py
@@ -0,0 +1,16 @@
+print("Simple Calculator")
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if op == '+':
+    print("Result:", num1 + num2)
+elif op == '-':
+    print("Result:", num1 - num2)
+elif op == '*':
+    print("Result:", num1 * num2)
+elif op == '/':
+    print("Result:", num1 / num2)
+else:
+    print("Invalid operator")
